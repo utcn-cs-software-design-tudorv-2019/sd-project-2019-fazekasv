b056df10552e1b17b0169b96f59b8092970e089e
Vlad Fazekas <vlad_fazekas@yahoo.com>
2019-05-23 01:04:07 +0300

    Add database configuration files for hibernate with postgresql

---
+++ code/drivingSchool/src/main/java/com/project/drivingSchool/config/HibernateConfig.java
@@ -0,0 +1,12 @@
+package com.project.drivingSchool.config;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConfig {
+
+	public static SessionFactory buildSessionFactory() {
+		SessionFactory sessionFactoryObj = new Configuration().configure()
+				.buildSessionFactory();
+		return sessionFactoryObj;
+	}
+}
